33e8ae04a7f8a9ebaf2bcc8cbe82a984948a46f6
xuhande <hande.xu@pixitek.com>
2015-06-24 17:30:05 +0800

    xuhande20150624-2 xhd

--- Theme/default/invitation/index.php
+++ Theme/default/invitation/index.php
@@ -134,19 +134,19 @@
             <div class="flipbook" > 
 
                 <div style="background: url(<?php echo v_theme_url() ?>/img/invitation/yao1.jpg);background-size:100% 100%;">
-                    <!--<img src="<?php echo v_theme_url() ?>/img/invitation/yao1.jpg" height="100%" width="100%"/>-->
+                    <img src="<?php echo v_theme_url() ?>/img/invitation/yao1.jpg" height="100%" width="100%"/>
                 </div>
                 <div style="background: url(<?php echo v_theme_url() ?>/img/invitation/yao4.jpg);background-size:100% 100%;">
-                    <!--<img src="<?php echo v_theme_url() ?>/img/invitation/yao4.jpg" height="100%" width="100%"/>-->
+                    <img src="<?php echo v_theme_url() ?>/img/invitation/yao4.jpg" height="100%" width="100%"/>
                 </div>
                 <div style="background: url(<?php echo v_theme_url() ?>/img/invitation/yao2.jpg);background-size:100% 100%;">
-                    <!--<img src="<?php echo v_theme_url() ?>/img/invitation/yao2.jpg"  height="100%" width="100%"/>-->
+                    <img src="<?php echo v_theme_url() ?>/img/invitation/yao2.jpg"  height="100%" width="100%"/>
                 </div>
                 <div style="background: url(<?php echo v_theme_url() ?>/img/invitation/yao3.jpg);background-size:100% 100%;">
-                    <!--<img src="<?php echo v_theme_url() ?>/img/invitation/yao3.jpg"  height="100%" width="100%"/>-->
+                    <img src="<?php echo v_theme_url() ?>/img/invitation/yao3.jpg"  height="100%" width="100%"/>
                 </div>
                 <div style="background: url(<?php echo v_theme_url() ?>/img/invitation/yao5.jpg);background-size:100% 100%;">
-                    <!--<img src="<?php echo v_theme_url() ?>/img/invitation/yao5.jpg" height="100%" width="100%"/>-->
+                    <img src="<?php echo v_theme_url() ?>/img/invitation/yao5.jpg" height="100%" width="100%"/>
                 </div>
 
             </div> 
