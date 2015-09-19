ab3396ebfd266ab53b0bfcfdecf69f517dcc7861
xuhande <hande.xu@pixitek.com>
2015-09-19 19:31:26 +0800

    20150919-1 xhd

--- Theme/default/store/index.php
+++ Theme/default/store/index.php
@@ -1,4 +1,9 @@
 <?php v_template_part(array("name" => "header", "path" => "Public")); ?> 
+<style>
+    body{
+       background: #380518; 
+    }
+</style>
 <div class="col-md-12 text-center" style="padding: 0; width: 100%;height:100%;background: #380518">
     <img src="<?php echo v_theme_url(); ?>/img/store.jpg"  width="100%" height="100%"/>
 </div>
