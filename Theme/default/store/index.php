02155bd45c692f90e532da83b29bea210769c645
xuhande <hande.xu@pixitek.com>
2015-09-25 09:52:55 +0800

    20150925-2 xhd

--- Theme/default/store/index.php
+++ Theme/default/store/index.php
@@ -4,8 +4,8 @@
        background: #380518; 
     }
 </style>
-<div class="col-md-12 text-center" style="padding: 0; width: 100%;height:100%;background: #380518">
-    <img src="<?php echo v_theme_url(); ?>/img/store.jpg"  width="100%" height="100%"/>
+<div class="col-md-12 text-center" style="padding: 0; width: 100%;height:1008px;background: #380518">
+    <img src="<?php echo v_theme_url(); ?>/img/store.jpg"  width="100%" height="1008px"/>
 </div>
    
 
