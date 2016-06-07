b4b789f796665aeffd5bc559c81544764fa3542f
aiakngs <song.kang@pixitek.com>
2016-06-07 12:56:31 +0800

    20160607-1 xhd

--- Theme/default/HkReturn/index_2016.php
+++ Theme/default/HkReturn/index_2016.php
@@ -291,8 +291,7 @@
             },
             //成功返回之后调用的函数             
             success: function (data) {
-                var obj = eval('(' + data + ')');
-                console.log(obj);
+                var obj = eval('(' + data + ')'); 
                 if (obj.code == "200") {
                     $('#chance').html(obj.chance);
                     if (lottery.id == "7") {
